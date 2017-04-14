28b3e88ec644965dc02560eb1224b34d7cd60432
kmezhoud <kmezhoud@gmail.com>
2017-04-14 20:26:30 +0100

    get pdf from Bioconductor

--- R/canceR_pdf.R
+++ R/canceR_pdf.R
@@ -15,6 +15,9 @@
 `canceR_Vignette` <-
     function() {
         tkgrab.release(window)
-        ViggIndex <- file.path(system.file("extdata",package="canceR"),"canceR.pdf")
-        Biobase::openPDF (ViggIndex) # opens the pdf
+        #ViggIndex <- file.path(system.file("extdata",package="canceR"),"canceR.pdf")
+        #Biobase::openPDF (ViggIndex) # opens the pdf
+        browseURL('http://www.bioconductor.org/packages/3.4/bioc/vignettes/canceR/inst/doc/canceR.pdf')
+        ## Morgan
+        #vignette(package="canceR", "canceR")
     }
